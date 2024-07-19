a2979088ada552df37ce333ed689aec0ab1ed690
Erica Sampson <ericabcherry@gmail.com>
2024-07-19 09:29:08 -0400

    Initial Commit

---
+++ 100DS_P7_iExpense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  100DS_P7_iExpense
+//
+//  Created by Erica Sampson on 2024-07-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 100DS_P7_iExpense/_00DS_P7_iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DS_P7_iExpenseApp.swift
+//  100DS_P7_iExpense
+//
+//  Created by Erica Sampson on 2024-07-19.
+//
+
+import SwiftUI
+
+@main
+struct _00DS_P7_iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
